95b052d788521731bda20d65d459625b5ea7cab9
Brett Tau <brettTau@bretts-macbook-pro.local>
2016-01-25 14:34:42 -0600

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Brett Tau on 1/25/16.
+//  Copyright © 2016 Brett Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
